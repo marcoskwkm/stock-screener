003dfdf997c586c89c9aba0a706cf2fd9ea62f2a
Marcos Kawakami <marcos.kawakami@vtex.com.br>
2020-06-21 15:37:24 -0300

    [app] Make filters collapsible

--- app/src/Filter.tsx
+++ app/src/Filter.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios, { AxiosResponse } from 'axios'
-import { Button, Form, Spinner } from 'react-bootstrap'
+import { Accordion, Button, Card, Form, Spinner } from 'react-bootstrap'
 
 import { SERVER_URL } from './constants'
 import SaveFilters from './SaveFilters'
@@ -65,57 +65,64 @@ const Filter: React.FC<Props> = ({ metrics }) => {
   }
 
   return (
-    <div className="mb-2">
-      <div className="flex mb-2">
-        <h5 className="my-auto">Applied filters</h5>
-        {loading && (
-          <Spinner
-            as="span"
-            animation="border"
-            variant="secondary"
-            size="sm"
-            className="my-auto ml-2"
-          />
-        )}
-      </div>
-
-      {userFiltersList && userFiltersList.length > 0 && (
-        <div className="pb-2">
-          <Form.Label>Saved filters</Form.Label>
-          <div>
-            {userFiltersList.map((userFilter) => (
-              <Button
-                className="p-0 mr-2 text-decoration-none"
-                variant="link"
-                key={userFilter.name}
-                onClick={() => handleSelectSavedFilter(userFilter)}
-              >
-                {userFilter.name}
-              </Button>
-            ))}
+    <Accordion className="mb-2">
+      <Card>
+        <Accordion.Toggle as={Card.Header} eventKey="0">
+          <div className="pointer flex">
+            <h5 className="my-auto">Applied filters</h5>
+            {loading && (
+              <Spinner
+                as="span"
+                animation="border"
+                variant="secondary"
+                size="sm"
+                className="my-auto ml-2"
+              />
+            )}
           </div>
-        </div>
-      )}
-      <Form.Group className="mb-2" controlId="selectedMetricsForm">
-        <Form.Label>Selected metrics</Form.Label>
-        <div>
-          {metrics.map(({ id, label }) => (
-            <Form.Check
-              inline
-              type="checkbox"
-              id={id}
-              key={id}
-              label={label}
-              checked={selectedMetrics.includes(id)}
-              onChange={(event: any) =>
-                handleSelectedMetricChange(id, event.target.checked)
-              }
-            />
-          ))}
-        </div>
-      </Form.Group>
-      <SaveFilters />
-    </div>
+        </Accordion.Toggle>
+        <Accordion.Collapse eventKey="0">
+          <Card.Body>
+            {userFiltersList && userFiltersList.length > 0 && (
+              <div className="pb-2">
+                <Form.Label>Saved filters</Form.Label>
+                <div>
+                  {userFiltersList.map((userFilter) => (
+                    <Button
+                      className="p-0 mr-2 text-decoration-none"
+                      variant="link"
+                      key={userFilter.name}
+                      onClick={() => handleSelectSavedFilter(userFilter)}
+                    >
+                      {userFilter.name}
+                    </Button>
+                  ))}
+                </div>
+              </div>
+            )}
+            <Form.Group className="mb-2" controlId="selectedMetricsForm">
+              <Form.Label>Selected metrics</Form.Label>
+              <div>
+                {metrics.map(({ id, label }) => (
+                  <Form.Check
+                    inline
+                    type="checkbox"
+                    id={id}
+                    key={id}
+                    label={label}
+                    checked={selectedMetrics.includes(id)}
+                    onChange={(event: any) =>
+                      handleSelectedMetricChange(id, event.target.checked)
+                    }
+                  />
+                ))}
+              </div>
+            </Form.Group>
+            <SaveFilters />
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    </Accordion>
   )
 }
 
